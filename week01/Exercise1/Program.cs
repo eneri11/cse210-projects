d47dbce039547bfa63dfd3d04bca71fae6618dfe
eneri11 <icostales@byupathway.edu>
2026-05-09 01:07:42 +0800

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,7 +8,7 @@ class Program
         string first = Console.ReadLine();
 
         Console.WriteLine("What is your last name? ");
-        string first = Console.ReadLine();
+        string last = Console.ReadLine();
 
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
