19f8836e5104f583485c8731f3e14e6825a1dc3c
eneri11 <icostales@byupathway.edu>
2026-05-12 19:55:54 +0800

    commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,20 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("This is in C#");
-
-        Console.WriteLine("What is your first name? ");
-        string first = Console.ReadLine();
-
-        Console.WriteLine("What is your last name? ");
-        string last = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
-        Console.WriteLine($"Your color is {color}");
+        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        
     }
 
 }
