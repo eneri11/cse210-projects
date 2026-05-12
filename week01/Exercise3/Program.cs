f1ab73a66f301e72358aa4f60f22d8c73ea7bb76
eneri11 <icostales@byupathway.edu>
2026-05-13 00:10:38 +0800

    commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // For Parts 1 and 2, where the user specified the number...
+                // Console.Write("What is the magic number? ");
+            // int magicNumber = int.Parse(Console.ReadLine());
+        
+            // For Part 3, where we use a random number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+        
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            }
+
+            // Display total guesses
+            Console.WriteLine("You guessed the number in {guessCount} attempts.");
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+
+        
+        Console.WriteLine("Thank you for playing!");
     }
 }
