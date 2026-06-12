67498c685a70da2317697b4e1a54c28ed8476d61
eneri11 <icostales@byupathway.edu>
2026-06-12 15:17:12 +0800

    commit

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,36 @@
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public string GetShortName()
+    {
+        return _shortName;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public abstract int RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public abstract string GetDetailsString();
+
+    public abstract string GetStringRepresentation();
+}
