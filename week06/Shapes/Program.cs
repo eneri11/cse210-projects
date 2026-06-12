8414da3ea8c139a9f4e9cb5785bf1a9cf3d385d9
eneri11 <icostales@byupathway.edu>
2026-06-12 16:07:11 +0800

    commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Test individual objects
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Color: {square.GetColor()}");
+        Console.WriteLine($"Area: {square.GetArea()}");
+
+        Console.WriteLine();
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Blue", 4));
+        shapes.Add(new Rectangle("Green", 6, 3));
+        shapes.Add(new Circle("Yellow", 2.5));
+
+        // Demonstrate polymorphism
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
